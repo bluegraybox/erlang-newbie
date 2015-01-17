26d1f3a084d4a9587af639c36f76b46f8e2dac00
Colin MacDonald <colin.r.macdonald@gmail.com>
2015-01-17 17:58:45 -0500

    Refactored mydiv snippet

--- code_snippets/mydiv.erl
+++ code_snippets/mydiv.erl
@@ -7,17 +7,27 @@ f(_X, 0) -> {error, "Divide by zero, dumbass."};
 f(X, Y) -> {X div Y, X rem Y}.
 
 main([Xstr, Ystr]) ->
-    case string:to_integer(Xstr) of
-        {error, Reason} -> io:format("Error parsing ~s: ~w~n", [Xstr, Reason]);
-        {X, ""} ->
-            case string:to_integer(Ystr) of
-                {error, Reason} -> io:format("Error parsing Y: ~w~n", [Reason]);
-                {Y, ""} ->
-                    case f(X, Y) of
-                        {error, Msg} -> io:format("Error: ~s~n", [Msg]);
-                        {Div, Mod} -> io:format("X:~w, Y:~w, div:~w, mod:~w~n", [X, Y, Div, Mod])
-                    end;
-                {_Y, YRest} -> io:format("What's with the '~s' crap?~n", [YRest])
-            end;
-        {_X, XRest} -> io:format("What's with the '~s' crap?~n", [XRest])
+    ParseY = fun (X) ->
+        BothOk = fun (Y) ->
+            display(X, Y)
+        end,
+        parseInt(Ystr, BothOk)
+    end,
+    parseInt(Xstr, ParseY).
+
+display(X, Y) ->
+    case f(X, Y) of
+        {error, Msg} -> io:format("Error: ~s~n", [Msg]);
+        {Div, Mod} -> io:format("X:~w, Y:~w, div:~w, mod:~w~n", [X, Y, Div, Mod])
+    end.
+
+parseInt(IntStr, Success) ->
+    case string:to_integer(IntStr) of
+        {error, Reason} ->
+            io:format("Error parsing ~s: ~w~n", [IntStr, Reason]);
+        {Val, ""} ->
+            Success(Val);
+        {Val, Rest} ->
+            io:format("What's with the '~s' crap?~n", [Rest]),
+            Success(Val)
     end.

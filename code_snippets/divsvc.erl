a6893a9ee0bd4728448bdc807c09b129ece9f961
Colin MacDonald <colin.r.macdonald@gmail.com>
2015-01-22 08:01:11 -0500

    Added supervisor sections and snippet

---
+++ code_snippets/divsvc.erl
@@ -0,0 +1,34 @@
+-module(divsvc).
+
+-export([init/0, supervisor/0, div_loop/0, get_div/2]).
+
+-define(SVC_NAME, divsvc).
+
+
+init() ->
+    spawn(?MODULE, supervisor, []).
+
+supervisor() ->
+    process_flag(trap_exit, true),
+    DivPid = spawn_link(?MODULE, div_loop, []),
+    register(?SVC_NAME, DivPid),
+    supervisor_loop().
+
+supervisor_loop() ->
+    receive
+        {'EXIT', _Pid, _Reason} ->
+            DivPid = spawn_link(?MODULE, div_loop, []),
+            register(?SVC_NAME, DivPid),
+            io:format("restarted divsvc with pid=~w~n", [DivPid])
+    end,
+    supervisor_loop().
+
+div_loop() ->
+    receive {FromPid, X, Y} -> FromPid ! X/Y end,
+    div_loop().
+
+get_div(X, Y) ->
+    ?SVC_NAME ! {self(), X, Y},
+    receive Resp -> Resp
+    after 3000 -> {error, "Timed out"}
+    end.

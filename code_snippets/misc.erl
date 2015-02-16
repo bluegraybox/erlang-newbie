98a2116581dceb22b5ce3fc6fdad07b562b2e469
Colin MacDonald <colin.macdonald@capitalone.com>
2015-02-16 15:57:55 -0500

    Snippet for httpc:request() matching
    Minor elaboration on example.

--- code_snippets/misc.erl
+++ code_snippets/misc.erl
@@ -1,7 +1,17 @@
 -module(misc).
 
--export([reassign/0]).
+-export([reassign/0, fetch/0]).
 
 reassign() ->
     X = 1,
     X = 2.
+
+fetch() ->
+    inets:start(),
+    Url = "http://bluegraybox.com/env.cgi",
+    case httpc:request(Url) of
+        {error, Message} -> io:format("error: ~s~n", [Message]);
+        {ok, {{_Proto, 200, _Desc}, _Headers, _Content}} -> io:format("success~n");
+        {ok, {{_Proto, Code, _Desc}, _Headers, _Content}} -> io:format("unexpected code: ~w~n", [Code]);
+        Unexpected -> io:format("unexpected respose: ~w~n", [Unexpected])
+    end.
